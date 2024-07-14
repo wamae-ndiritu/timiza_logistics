bab29c615d9118ebf9923b24cac8e24c9c05d0e3
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-14 15:16:04 +0300

    Add user update profile feature

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Driver = require("../models/Driver");
 const Loader = require("../models/Loader");
-const { isAdmin } = require("../middleware/auth");
+const { isAdmin, verify } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -26,7 +26,7 @@ router.post("/login", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    if (role === "driver" || role === "loader" ) {
+    if (role === "driver" || role === "loader") {
       isMatch = await user.user.comparePassword(password);
     } else {
       isMatch = await user.comparePassword(password);
@@ -91,7 +91,7 @@ router.post("/login", async (req, res) => {
 
         res.json({
           token,
-          userInfo: userDetails
+          userInfo: userDetails,
         });
       }
     );
@@ -100,6 +100,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Register admin
 router.post("/register/admin", async (req, res) => {
   const { email, password } = req.body;
 
@@ -179,14 +180,215 @@ router.post("/register", isAdmin, async (req, res) => {
   }
 });
 
-
-// List users
-router.get('/', async (req, res) => {
-  try {
-    
-  } catch (error) {
-    
+// Admin get all users
+router.get("/", isAdmin, async (req, res) => {
+  const type = req.query.type;
+  let usersList = [];
+  if (type === "drivers") {
+    usersList = await Driver.find({}).populate("user").sort({ _id: -1 });
+  } else if (type === "loaders") {
+    usersList = await Loader.find({}).populate("user").sort({ _id: -1 });
   }
-})
+  if (type === "admins") {
+    usersList = await User.find({ role: "admin" }).sort({ _id: -1 });
+  }
+  res.status(200).json(usersList);
+});
+
+// User get their profile
+router.get("/profile", verify, async (req, res) => {
+  const type = req.user.role;
+  try {
+    let user;
+
+    if (type === "driver") {
+      user = await Driver.findOne({ user: req.user.id }).populate("user");
+    } else if (type === "loader") {
+      user = await Loader.findOne({ user: req.user.id }).populate("user");
+    }else{
+      return res.status(400).json({message: "Invalid user type!"})
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    let profile = {
+      _id: user.user._id,
+      email: user.user.email,
+      role: user.user.role,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+      nationalId: user.nationalId,
+      drivingLicense: user.drivingLicense,
+      nationalIdCopy: user.nationalIdCopy,
+    };
+
+    res.status(200).json(profile);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// User update  their profile (password only)
+router.put("/profile", verify, async (req, res) => {
+  const { oldPassword, newPassword, confirmPassword } = req.body;
+
+  try {
+    // Find the user by ID
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if old password is correct
+    const isMatch = await user.comparePassword(oldPassword);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Old password is incorrect" });
+    }
+
+    // Check if new password and confirm password match
+    if (newPassword !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "New password and confirm password do not match" });
+    }
+
+    // Set the new password (it will be hashed by the pre-save hook)
+    user.password = newPassword;
+
+    // Save the user with the updated password
+    await user.save();
+
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// User update nationalIdCopy and drivingLicenseCopy (normal user)
+router.put("/profile/documents", verify, async (req, res) => {
+  const { nationalIdCopy, drivingLicenseCopy } = req.body;
+
+  try {
+    // Find the user by ID
+    const type = req.user.role;
+    let user;
+    if (type === "driver") {
+      user = await Driver.findOne({ user: req.user.id }).populate("user");
+    } else if (type === "loader") {
+      user = await Loader.findOne({ user: req.user.id }).populate("user");
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Documents not required for admin!" });
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update nationalIdCopy and drivingLicenseCopy if they are initially null
+    if (type === "driver" || type === "loader") {
+      if (!user.nationalIdCopy && nationalIdCopy) {
+        user.nationalIdCopy = nationalIdCopy;
+      } else{
+        return res.status(400).json({
+          message: "Updating documents may only be initiated by the admin!",
+        });
+      }
+      if (!user.drivingLicense && drivingLicenseCopy) {
+        user.drivingLicense = drivingLicenseCopy;
+      }else{
+        return res.status(400).json({
+          message: "Updating documents may only be initiated by the admin!",
+        });
+      }
+      await user.save();
+    }
+
+    res.status(200).json({ message: "Documents updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Admin update user profile (all details)
+router.put("/profile/admin/:id", isAdmin, async (req, res) => {
+  const { id } = req.params;
+  const { fullName, email, role, phoneNumber, nationalId } = req.body;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user details
+    if (email) user.email = email;
+    if (role) user.role = role;
+
+    await user.save();
+
+    // Update profile details based on role
+    let profile;
+    if (user.role === "driver") {
+      profile = await Driver.findOne({ user: user._id });
+      if (profile) {
+        if (phoneNumber) profile.phoneNumber = phoneNumber;
+        if (fullName) profile.fullName = fullName;
+        if (nationalId) profile.nationalId = nationalId;
+        await profile.save();
+      }
+    } else if (user.role === "loader") {
+      profile = await Loader.findOne({ user: user._id });
+      if (profile) {
+        if (phoneNumber) profile.phoneNumber = phoneNumber;
+        if (fullName) profile.fullName = fullName;
+        if (nationalId) profile.nationalId = nationalId;
+        await profile.save();
+      }
+    }
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Admin delete user
+router.delete("/:id", isAdmin, async (req, res) => {
+  const { type } = req.query;
+
+  try {
+    let user;
+    if (type === "driver") {
+      user = await Driver.findOne({user: req.params.id}).populate("user");
+      if (user) {
+        await User.findByIdAndDelete(user.user._id);
+        await Driver.findByIdAndDelete(req.params.id);
+      }
+    } else if (type === "loader") {
+      user = await Loader.findOne({user: req.params.id}).populate("user");
+      if (user) {
+        await User.findByIdAndDelete(user.user._id);
+        await Loader.findByIdAndDelete(req.params.id);
+      }
+    } else {
+      await User.findByIdAndDelete(req.params.id);
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User profile deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
