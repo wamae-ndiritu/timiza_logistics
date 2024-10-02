4653d7a651b1eee032582709766d121ecc4eec92
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-10-02 18:29:07 +0300

    Configure app settings

--- mobile-client-app/lib/baseUrl.js
+++ mobile-client-app/lib/baseUrl.js
@@ -1,3 +1,2 @@
 // export const END_POINT = "http://192.168.88.203:3000/api/v1";
-export const END_POINT = "http://41.89.89.17:3000/api/v1";
-// export const END_POINT = "http://192.168.95.91:3000/api/v1";
+export const END_POINT = "https://timiza-logistics-server.onrender.com/api/v1";
