12ca2356b131c3ccf597e7ab55395ae179e8aa62
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-11-10 02:17:53 +0300

    Add index screen loading spinner

--- timiza-app/app/index.jsx
+++ timiza-app/app/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, ScrollView, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "../constants/images";
-import CustomButton from "../components/CustomButton";
 import { router } from "expo-router";
 
 export default function IndexScreen() {
+  const [dotIndex, setDotIndex] = useState(0);
+
+  useEffect(() => {
+    // Redirect to the sign-in page after 5 seconds
+    const timer = setTimeout(() => {
+      router.push("/sign-in");
+    }, 5000);
+
+    // Interval for dot animation
+    const dotTimer = setInterval(() => {
+      setDotIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 500);
+
+    // Cleanup timers on component unmount
+    return () => {
+      clearTimeout(timer);
+      clearInterval(dotTimer);
+    };
+  }, []);
+
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView contentContainerStyle={{ height: "100%" }}>
@@ -15,12 +34,18 @@ export default function IndexScreen() {
             className='max-w-[320px] w-full h-[200px]'
             resizeMode='contain'
           />
-          <CustomButton
-            title='Continue'
-            handlePress={() => router.push("/sign-in")}
-            containerStyles='w-full mt-7'
-            textStyles="text-xl text-white-100"
-          />
+
+          {/* Three-Dot Loading Indicator */}
+          <View className='flex-row mt-7 space-x-2'>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <View
+                key={index}
+                className={`h-3 w-3 rounded-full ${
+                  dotIndex === index ? "bg-secondary" : "bg-gray-300"
+                }`}
+              />
+            ))}
+          </View>
         </View>
       </ScrollView>
       <StatusBar backgroundColor='#E0DEDB' style='light' />
