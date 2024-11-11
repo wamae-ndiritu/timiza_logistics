492c54c9316f6ad7f4071b9e115437dab47cf2f8
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-11-11 22:49:14 +0300

    Connect to remote server

--- timiza-app/lib/baseUrl.js
+++ timiza-app/lib/baseUrl.js
@@ -1,3 +1,1 @@
-// export const END_POINT = "https://timiza-logistics-server.onrender.com/api/v1";
-
-export const END_POINT = "http://192.168.88.152:3000/api/v1";
+export const END_POINT = "https://timiza-logistics-server.onrender.com/api/v1";
