b911289a337bb35393f135da1daeaf9dd946fbe8
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-11-11 22:23:49 +0300

    Hadle soft deletion

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -100,7 +100,7 @@ router.post("/login", async (req, res) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: "20s" },
+      { expiresIn: "30d" },
       (err, token) => {
         if (err) throw err;
 
